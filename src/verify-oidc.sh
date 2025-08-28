f21c09b48421237663192711cabfd8e8bd10e1f1
Priya Kandababu <priyakandababu@priyas-MacBook-Pro.local>
2025-08-29 09:42:17 +1000

    test OIDC

--- src/verify-oidc.sh
+++ src/verify-oidc.sh
@@ -5,6 +5,6 @@ set -eu
 
 singing_key=123
 echo "Verifying OIDC token..."
-$ buildkite-agent oidc request-token --audience sts.amazonaws.com
-$ buildkite-agent oidc request-token ... --claim $singing_key
-$ buildkite-agent oidc request-token --audience sts.amazonaws.com
+buildkite-agent oidc request-token --audience sts.amazonaws.com
+buildkite-agent oidc request-token ... --claim $singing_key
+buildkite-agent oidc request-token --audience sts.amazonaws.com
