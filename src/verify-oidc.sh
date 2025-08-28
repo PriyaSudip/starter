11199f22aefb06c3e1706b6ea1d2757c64b5b0f0
Priya Kandababu <priyakandababu@priyas-MacBook-Pro.local>
2025-08-29 09:51:40 +1000

    test OIDC

--- src/verify-oidc.sh
+++ src/verify-oidc.sh
@@ -9,6 +9,9 @@ buildkite-agent oidc request-token --audience sts.amazonaws.com
 
 echo "Requesting OIDC token with claim..."
 
+buildkite-agent oidc request-token ... --claim "organization_id"
+echo "Requesting OIDC token with claim signing"
+
 buildkite-agent oidc request-token ... --claim $singing_key
 
 echo    "Requesting OIDC token without claim..."
