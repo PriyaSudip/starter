caa68da629f5a574f969c4b1b52ccbeb0b770157
Priya Kandababu <priyakandababu@priyas-MacBook-Pro.local>
2025-10-08 16:26:29 +1100

    test

--- src/push_image_ecr.sh
+++ src/push_image_ecr.sh
@@ -16,8 +16,6 @@ export AWS_DEFAULT_REGION=${APP_ACCOUNT_REGION:-"ap-southeast-2"}
 echo "--- Tagging and pushing to Account app account $AWS_DEFAULT_REGION"
 
 
-aws ecr get-login-password --region ap-southeast-2 |
-docker login --username AWS --password-stdin 097340723131.dkr.ecr.ap-southeast-2.amazonaws.com
 
 docker tag my-app:$BUILDKITE_BUILD_NUMBER 097340723131.dkr.ecr.ap-southeast-2.amazonaws.com/my-app:latest
 docker push 097340723131.dkr.ecr.ap-southeast-2.amazonaws.com/my-app:latest
